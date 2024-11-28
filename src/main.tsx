788c1329b43a25c23e1ba0d58b42f50e53a9bf25
Jekz <gabrielrodrigues23@proton.me>
2024-11-28 09:02:11 -0300

    Add root element debugging to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -23,8 +23,13 @@ ChartJS.register(
   Legend
 )
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  console.error('Root element not found!');
+} else {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+}
